78cfa522c24576dd8b8d91b1597670bdb8491e08
inspire12 <ox4443@naver.com>
2025-12-02 16:21:47 +0900

    feat: upstage embedding api 추가

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get("/hello")
 def hello():
     return {"message": "Hello FastAPI!"}
+
+@app.post("/query")
+def query(message:str):
+    # pip install openai
+    from openai import OpenAI  # openai==1.52.2
+
+    client = OpenAI(
+        api_key="up_CQlRUTuZcA4l3YwnD7g9dAdT74hXw",
+        base_url="https://api.upstage.ai/v1"
+    )
+    response = client.embeddings.create(
+        input=message,
+        model="embedding-query"
+    )
+
+    print(response.data[0].embedding)
